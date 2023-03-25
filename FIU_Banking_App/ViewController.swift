22878496ab46ffab7aba72a1db571447b77d76f1
Ariadna <98125876+NairFran@users.noreply.github.com>
2023-03-25 16:35:48 -0400

    Start
    Basic layout of application

---
+++ FIU_Banking_App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FIU_Banking_App
+//
+//  Created by Walter Franchino on 3/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
