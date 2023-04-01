605247c7a68201d226420fb6047857f47ea34b4f
Ariadna <98125876+NairFran@users.noreply.github.com>
2023-04-01 16:28:39 -0500

    Account page Update
    Page now shows the current users Username.

--- lab-insta-parse/AccountViewController.swift
+++ lab-insta-parse/AccountViewController.swift
@@ -8,25 +8,18 @@
 import UIKit
 
 class AccountViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUsername()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUsername(){
+        usernameLabel.text = User.current?.username
     }
-    */
 
-    
     @IBAction func onLogOutTapped(_ sender: Any) {
         showConfirmLogoutAlert()
     }
